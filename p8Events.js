aa7b4bd0b616af2dca732047f5905e21930354d7
danocolombo@google.com <GR0mans1212!>
2021-02-18 08:32:42 -0500

    events add and save created - not functional

--- p8Events.js
+++ p8Events.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+const crypto = require('crypto');
 var dynamo = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
 /**
@@ -44,6 +45,75 @@ exports.handler = async (event, context, callback) => {
                 body: eData,
             };
             return response;
+        case 'saveEvent':
+            //this will save the event to the database,
+            //but we do require some values before
+            //taking action
+            //========================================
+            let errMsg = {};
+            requirementsMet = true;
+            if (!event.payload.Item.location.hasOwnProperty('name')) {
+                requirementsMet = false;
+                errMsg = { locaton: { name: 'not satisfied' } };
+            }
+            if (!event.payload.Item.location.hasOwnProperty('state')) {
+                requirementsMet = false;
+                errMsg = { location: { state: 'not satisfied' } };
+            }
+            if (!event.payload.Item.location.hasOwnProperty('city')) {
+                requirementsMet = false;
+                errMsg = { location: { city: 'not satisfied' } };
+            }
+            if (!event.payload.Item.hasOwnProperty('startTime')) {
+                requirementsMet = false;
+                errMsg = { startTime: 'not satisfied' };
+            }
+            if (!event.payload.Item.hasOwnProperty('endTime')) {
+                requirementsMet = false;
+                errMsg = { endTime: 'not satisfied' };
+            }
+            if (!event.payload.Item.hasOwnProperty('eventDate')) {
+                requirementsMet = false;
+                errMsg = { eventDate: 'not satisfied' };
+            }
+            if (!event.payload.Item.contact.hasOwnProperty('name')) {
+                requirementsMet = false;
+                errMsg = { contact: { name: 'not satisfied' } };
+            }
+            if (!event.payload.Item.coordinator.hasOwnProperty('name')) {
+                requirementsMet = false;
+                errMsg = { coordinator: { name: 'not satisfied' } };
+            }
+            if (!event.payload.Item.coordinator.hasOwnProperty('uid')) {
+                requirementsMet = false;
+                errMsg = { coordinator: { uid: 'not satisfied' } };
+            }
+            if (requirementsMet) {
+                //check to see if the Item has id or is new
+                if (!event.payload.Item.hasOwnProperty('uid')) {
+                    let newId = getUniqueId();
+                    console.log('newId:' + newId);
+                    event.payload.Item.uid = newId;
+                }
+
+                event.payload.TableName = 'p8Events';
+                let eventResponse = null;
+                try {
+                    eventResponse = await dynamo.put(event.payload).promise();
+                    return event.payload;
+                } catch {
+                    return eventResponse;
+                }
+            } else {
+                payload.status = '406';
+                payload.body.message =
+                    'Pate: Request Not Acceptable. (' +
+                    operation +
+                    ') Requirements Not Met';
+                payload.errorMessage = errMsg;
+                return payload;
+            }
+            break;
         case 'echo':
             callback(null, 'Success');
             break;
@@ -110,4 +180,20 @@ async function getActiveEvents() {
     } catch (err) {
         console.log('FAILURE in dynamoDB call', err.message);
     }
+}
+function getUniqueId() {
+    //this generates a unique ID based on this specific time
+    // Difining algorithm
+    const algorithm = 'aes-256-cbc';
+    // Defining key
+    const key = crypto.randomBytes(32);
+    // Defining iv
+    const iv = crypto.randomBytes(16);
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+    //get the current time...
+    let n = Date.now();
+    let encrypted = cipher.update(n.toString());
+    // Using concatenation
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+    return encrypted.toString('hex');
 }
