f129a9b57b552d3fb5d1769a63138ffc9a778583
danocolombo <danocolombo@gmail.com>
2021-03-31 06:51:01 -0400

    maintainEventNumbers almost done

--- p8Events.js
+++ p8Events.js
@@ -305,7 +305,7 @@ function GetAscendSortOrder(prop) {
     };
 }
 
-function maintainEventNumbers(request) {
+async function maintainEventNumbers(request) {
     /*
         it should look like this....
         "request": {
@@ -322,55 +322,114 @@ function maintainEventNumbers(request) {
 
     */
     let adjustments = request.adjustments;
-    let adjReg = request.adjustments.hasOwnProperty('registrationCount');
-    let adjMeal = request.adjustments.hasOwnProperty('mealCount');
-    let adjAttend = request.adjustments.hasOwnProperty('attendance');
-    let adjServed = request.adjustments.hasOwnProperty('mealsServed');
+    const ADJ_REGISTRATIONS = request.adjustments.hasOwnProperty('registrationCount');
+    const ADJ_MEAL_PLAN = request.adjustments.hasOwnProperty('mealCount');
+    const ADJ_ATTENDANCE = request.adjustments.hasOwnProperty('attendance');
+    const ADJ_MEALS_FED = request.adjustments.hasOwnProperty('mealsServed');
     //now get the event
-    let resp = getEvent(request.uid);
+    let theStoredEvent = {};
+    const uParams = {
+        TableName: 'p8Events',
+        KeyConditionExpression: 'uid = :v_uid',
+        ExpressionAttributeValues: {
+            ':v_uid': request.uid,
+        },
+    };
+    try {
+        // console.log('BEFORE dynamo query');
+        // console.log('uid:' + var1);
+        theStoredEvent = await dynamo.query(uParams).promise();
+        // console.log(data);
+        // return data;
+    } catch (err) {
+        console.log('FAILURE in dynamoDB call', err.message);
+        return {'ERROR': 'trying to get original event'}
+    }
+    // console.log('VVVVVVVVVVVVVVVVV');
+    // const util = require('util');
+    // console.log('theStoredEvent:  \n' + util.inspect(theStoredEvent, { showHidden: false, depth: null }));
+    // console.log('^^^^^^^^^^^^^^^^^');
+    // console.log('==> registrations:' + theStoredEvent.Items[0].registrations);
+    const WAS_REGISTRATIONS = theStoredEvent.Items[0].registrations;
+    // console.log('==> mealCount:' + theStoredEvent.Items[0].meal.mealCount);
+    const WAS_MEAL_PLAN = theStoredEvent.Items[0].meal.mealCount;
+    // console.log('==> attendance:' + theStoredEvent.Items[0].attendance);
+    const WAS_ATTENDANCE = theStoredEvent.Items[0].attendance;
+    // console.log('==> mealsServed:' + theStoredEvent.Items[0].meal.mealsServed);
+    const WAS_MEALS_FED = theStoredEvent.Items[0].meal.mealsServed;
 
+    // console.log('WAS_REGISTRATIONS:'+WAS_REGISTRATIONS);
+    // console.log('WAS_MEAL_PLAN:'+WAS_MEAL_PLAN);
+    // console.log('WAS_ATTENDANCE:'+WAS_ATTENDANCE);
+    // console.log('WAS_MEALS_FED:'+WAS_MEALS_FED);
+
+    // console.log('ADJ_REGISTRATIONS:'+ADJ_REGISTRATIONS);
+    // console.log('ADJ_MEAL_PLAN:'+ADJ_MEAL_PLAN);
+    // console.log('ADJ_ATTENDANCE:'+ADJ_ATTENDANCE);
+    // console.log('ADJ_MEALS_FED:'+ADJ_MEALS_FED);
     //!!!!!!!!!!!!!!!!!!!!!!
     // need error handling in case bad uid provided.
-    let theEvent = resp.body.Item;
-    return theEvent;
-    if (adjReg) {
-        //need to update registration value
-        // console.log(
-        //     'theEvent.registrations:' + parseInt(theEvent.registations)
-        // );
-        // let currentValue = parseInt(theEvent.registrations);
-        console.log('theEvent.registrations:' + theEvent.registrations);
-        let updatedValue = parseInt(theEvent.registrations) + parseInt(adjReg);
-        theEvent.registrations = updatedValue;
+    let workingEvent = theStoredEvent.Items[0];
+    
+    
+    
+    workingEvent.meal.mealsServed = parseInt(WAS_MEALS_FED) + ADJ_MEALS_FED;
+    // console.log('workingEvent:  \n' + util.inspect(workingEvent, { showHidden: false, depth: null }));
+    
+    
+    if (ADJ_REGISTRATIONS) {
+        const DELTA_REGISTRATIONS = parseInt(request.adjustments.registrationCount);
+        if((parseInt(WAS_REGISTRATIONS) + DELTA_REGISTRATIONS)< 1) {
+            workingEvent.registrations = 0;
+        }else{
+            workingEvent.registrations = parseInt(WAS_REGISTRATIONS) + DELTA_REGISTRATIONS;
+        }
     }
-    // if (adjMeal) {
-    //     //need to update registration value
-    //     // let currentValue = parseInt(theEvent.meal.mealCount);
-    //     let updatedValue =
-    //         parseInt(theEvent.meal.mealCount) + parseInt(adjMeal);
-    //     theEvent.meal.mealCount = updatedValue;
-    // }
-    // if (adjAttend) {
-    //     //need to update registration value
-    //     // let currentValue = parseInt(theEvent.meal.mealCount);
-    //     let updatedValue = parseInt(theEvent.attendance) + parseInt(adjAttend);
-    //     theEvent.attendance = updatedValue;
-    // }
-    // if (adjServed) {
-    //     //need to update registration value
-    //     // let currentValue = parseInt(theEvent.meal.mealCount);
-    //     let updatedValue =
-    //         parseInt(theEvent.meal.mealsServed) + parseInt(adjServed);
-    //     theEvent.meal.mealsServed = updatedValue;
-    // }
+    if (ADJ_MEAL_PLAN) {
+        const DELTA_MEAL_PLAN = parseInt(request.adjustments.mealCount);
+        if((parseInt(WAS_MEAL_PLAN) + DELTA_MEAL_PLAN)< 1) {
+            workingEvent.meal.mealCount = 0;
+        }else{
+            workingEvent.meal.mealCount = parseInt(WAS_MEAL_PLAN) + DELTA_MEAL_PLAN;
+        }
+    }
+    if (ADJ_ATTENDANCE) {
+        const DELTA_ATTENDANCE = parseInt(request.adjustments.attendance);
+        if((parseInt(WAS_ATTENDANCE) + DELTA_ATTENDANCE)< 1) {
+            workingEvent.attendance = 0;
+        }else{
+            workingEvent.attendance = parseInt(WAS_ATTENDANCE) + DELTA_ATTENDANCE;
+        }
+    }
+    if (ADJ_MEALS_FED) {
+        const DELTA_MEALS_FED = parseInt(request.adjustments.mealsServed);
+        if((parseInt(WAS_MEALS_FED) + DELTA_MEALS_FED)< 1) {
+            workingEvent.meal.mealsServed = 0;
+        }else{
+            workingEvent.meal.mealsServed = parseInt(WAS_MEALS_FED) + DELTA_MEALS_FED;
+        }
+    }
+    // return workingEvent;
     //now save the event, build the request
-    let theRequest = {
+    
+    const UPDATED_REGISTRATION = {
         TableName: 'p8Events',
-        Item: theEvent,
+        Item: workingEvent,
     };
-    // async function updateTheRegistration() {
-    //     let results = await dynamo.put(theRequest).promise();
-    // }
+    console.log('vvvvvvvvvvvvvvvvvv');
+    console.log(UPDATED_REGISTRATION);
+    console.log('^^^^^^^^^^^^^^^^^');
+    try {
+        async function updateTheRegistration() {
+            let results = await dynamo.put(UPDATED_REGISTRATION).promise();
+            console.log('it worked');
+            return UPDATED_REGISTRATION;
+        }
+        updateTheRegistration();
+    }catch(error){
+        console.log('ERROR DURING UPDATE')
+        return error;
+    }
     // updateTheRegistration();
 
     // let response = {
@@ -379,5 +438,5 @@ function maintainEventNumbers(request) {
     //     attend: adjAttend,
     //     fed: adjServed,
     // };
-    return theRequst;
+    // return results;
 }
